71f5e166aad49cedfe70a2b76e756056d523b771
Zachary <zackadee@gmail.com>
2019-04-06 11:59:32 -0700

    first commit

---
+++ Machine.java
@@ -0,0 +1,17 @@
+import org.jfugue.midi.MidiFileManager;
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+
+public class Machine
+{
+
+   private boolean active;
+   
+   private String 
+   
+   public String main(String[] args)
+   {
+      if(active)
+   }
+   
+}
